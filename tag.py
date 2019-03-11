2a9ff04a0994e4096a8ad53c082797c118ba00a7
Sagar Saxena <sagardsaxena@gmail.com>
2019-03-11 19:25:45 -0400

    Visualize Data

--- tag.py
+++ tag.py
@@ -2,6 +2,7 @@ import sqlite3
 import argparse
 from progress.bar import Bar
 import cv2 as cv
+import os
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-d', '--database', help="Path To SQLite Database", required=True)
@@ -10,24 +11,27 @@ conn = None
 curs = None
 
 def connect():
-	conn = sqlite3.connect(ap['database'])
+	global conn, curs
+	conn = sqlite3.connect(args['database'])
 	curs = conn.cursor()	
 
 def tag(datestr, location, compid, userid):
-	if not curs: pass
-	curs.execute("update records set userid=? where compid=? and location=? and datestr=?", (userid,compid,location,datestr))
+	if not curs: return
+	curs.execute("update records set userid=? where compid=? and loc=? and datetime like ?", (userid,compid,location,datestr))
 
-def vis(datestr, loc, compid):
-	if not curs: pass
-	rows = curs.execute("select image from records where compid=? and location=? and datestr=?", (compid,location,datestr))
+def vis(datestr, location, compid):
+	os.chdir("tiny")
+	if not curs: return
+	rows = curs.execute("select image from records where compid=? and loc=? and datetime like ?", (compid,location,datestr))
 	for row in rows:
 		img = cv.imread(row[0])
 		cv.imshow("image", img)
-		cv.waitkey(0)
+		cv.waitKey(0)
 	cv.destroyAllWindows()	
-	
+	os.chdir('..')
+
 def close():
-	if not conn: pass
+	if not conn: return
 	conn.commit()
 	conn.close()
 
@@ -46,8 +50,9 @@ def vis_inp():
 	vis(datestr, loc, compid)
 	return True
 	
-def __main__():
+if __name__ == "__main__":
 	connect()
+	print(conn,curs)
 	while True: 
 		inp = int(input("0 To Tag\n1 To Visualize\n-1 To Quit\nCommand: "))
 		if(inp == -1): break 
